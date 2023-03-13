832a4155f0a4d854d8d955c7522f41313010a136
Vsevolod Slavianinov <a0974377355@gmail.com>
2023-03-13 21:34:37 +0200

    Topbar component created, router added

--- src/pages/global/Topbar.js
+++ src/pages/global/Topbar.js
@@ -9,7 +9,7 @@ import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchIcon from "@mui/icons-material/Search";
 
-export default function Topbar() {
+const Topbar = () => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     const colorMode = useContext(ColorModeContext);
@@ -49,4 +49,6 @@ export default function Topbar() {
             </Box>
         </Box>
     );
-}
+};
+
+export default Topbar;

e910eb8f7796c2a00c419c5008dfa01cd00d2220
Vsevolod Slavianinov <a0974377355@gmail.com>
2023-03-14 19:13:46 +0200

    data grids added: team, contacts & invoices

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { Routes, Route } from "react-router-dom";
 import Topbar from "./pages/global/Topbar";
 import Sidebar from "./pages/global/Sidebar";
 import Dashboard from "./pages/dashboard";
-// import Team from "./pages/team";
-// import Invoices from "./pages/invoices";
-// import Contacts from "./pages/contacts";
+import Team from "./pages/team";
+import Invoices from "./pages/invoices";
+import Contacts from "./pages/contacts";
 // import Bar from "./pages/bar";
 // import Form from "./pages/form";
 // import Line from "./pages/line";
@@ -29,10 +29,10 @@ function App() {
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team />} />
+              <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
+              {/* <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
